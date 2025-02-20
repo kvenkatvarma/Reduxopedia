1dc7844e657abf1c9c6ee85a7428878aca6c9d47
Venkat varma <kvenkatvarma@gmail.com>
2025-02-20 12:29:40 +0530

    Add Destination Slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./slice/counterSlice";
+import { destinationReducer } from "./slice/destinationSlice";
 
 export const store = configureStore({
     reducer:{
         counterStore:counterReducer,
+        destinationStore:destinationReducer,
     }
 });
-
+console.log(store.getState());
