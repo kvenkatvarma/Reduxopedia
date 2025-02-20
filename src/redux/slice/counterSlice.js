d51a2e1f6673ef5ccf8999e03ca0c30196959252
Venkat varma <kvenkatvarma@gmail.com>
2025-02-20 11:42:40 +0530

    Create First reducer and action

---
+++ src/redux/slice/counterSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState ={count : 0 };
+
+export const counterslice = createSlice({
+      name: "counter",
+      initialState: initialState,
+      reducers : {
+        increment :(state)=>{
+            state.count+=1;
+        },
+        decrement :(state)=>{
+            state.count-=1;
+        }
+      }
+});
+export const {increment,decrement} = counterslice.actions;
+export const counterReducer = counterslice.reducer;
