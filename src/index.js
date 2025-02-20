b57b13e115041552aa7261d44871b3aef773b1e1
Venkat varma <kvenkatvarma@gmail.com>
2025-02-20 12:42:25 +0530

    Display Destinations

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import Header from './app/layout/Header';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import Counter from './app/Components/Counter';
+import DestinationList from './app/Components/DestinationList';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div className='text-white'>
-    <Provider store={store}>
-        <Header/>
-        <Counter/>
-    </Provider>
-    
+        <Provider store={store}>
+            <Header/>
+            <Counter/>
+            <div className='p-4 border text-center'>
+                <h4 className='text-success'>Destination List</h4>  
+                <DestinationList/>
+            </div>        
+        </Provider>    
     </div>
 );
